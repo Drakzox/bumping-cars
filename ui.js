1e822f1283d161cbd2f4ca8afeb826afab4b9f14
Drakzox <orikiyehiel@gmail.com>
2026-03-17 22:58:26 +0200

    Make cursor orb bigger

--- ui.js
+++ ui.js
@@ -328,7 +328,7 @@ const UI = (() => {
         trailerY += (mouseY - trailerY) * 0.15;
         
         if (trailer) {
-            trailer.style.transform = `translate(${trailerX - 100}px, ${trailerY - 100}px)`;
+            trailer.style.transform = `translate(${trailerX - 200}px, ${trailerY - 200}px)`;
         }
         requestAnimationFrame(animateCursor);
     }
